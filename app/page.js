13a075d255f996a113762dbd51a0ad122ea39e38
piusCode <obliepius13@gmail.com>
2025-05-09 12:25:42 +0000

    Update page component to display a personalized message

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <div>My name is Pius Oblie. I am The best teacher in the world</div>;
 };
 
 export default page;
