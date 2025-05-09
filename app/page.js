0f7d1c09f32bc693a6fe9d69ecc805eaa43185c5
piusCode <obliepius13@gmail.com>
2025-05-09 12:37:39 +0000

    Enhance page layout with welcoming message and image for BlissCollege

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>My name is Pius Oblie. I am The best teacher in the world</div>;
+  return (
+    <div className="w-full min-h-screen bg-amber-200 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline my-4">
+        Welcome To BlissCo 2025
+      </h1>
+      <p className="text-lg">
+        This is BlissCollege computer science and ICT students{" "}
+      </p>
+
+      <img src="/blissCo.jpg" />
+    </div>
+  );
 };
 
 export default page;
